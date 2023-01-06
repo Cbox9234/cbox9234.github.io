c04d20de6d6f166213a9a1c076257f869055af8d
epicX67 <souravgain605@gmail.com>
2023-01-06 23:07:32 +0530

    added game icons in hero

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,12 @@
 import Head from "next/head";
 import Image from "next/image";
 import logo from "../res/logo.png";
+import image1 from "../res/g1.png";
+import image2 from "../res/g2.png";
+import image3 from "../res/g3.png";
+import image4 from "../res/g4.png";
+import image5 from "../res/g5.png";
+import image6 from "../res/g6.png";
 
 export default function Home() {
   return (
@@ -25,7 +31,14 @@ export default function Home() {
                 <button className="heroBtn">Contact</button>
               </div>
             </div>
-            <div className="rCont"></div>
+            <div className="rCont">
+              <Image className="gridItems" src={image1} alt="logo" />
+              <Image className="gridItems" src={image2} alt="logo" />
+              <Image className="gridItems" src={image3} alt="logo" />
+              <Image className="gridItems" src={image4} alt="logo" />
+              <Image className="gridItems" src={image5} alt="logo" />
+              <Image className="gridItems" src={image6} alt="logo" />
+            </div>
           </div>
         </section>
       </main>
