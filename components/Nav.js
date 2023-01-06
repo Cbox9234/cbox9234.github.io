51ec4556b55898e434019879c1de38ca618623af
epicX67 <souravgain605@gmail.com>
2023-01-06 22:35:38 +0530

    nav bar now expand up to 1200px

--- components/Nav.js
+++ components/Nav.js
@@ -8,19 +8,21 @@ import NavDetails from "./Nav/NavDetails";
 export default function Nav() {
   return (
     <div className="nav">
-      <div className="logo">
-        <Image alt="Logo" src={Logo} />
-        LEGiON Platfoms
-      </div>
-      <div className="links">
-        <NavItem title="Products" ExpandedElem={<NavDetails />} />
-        <NavItem title="Company" ExpandedElem={<NavDetails />} />
-        <NavItem title="Resources" ExpandedElem={<NavDetails />} />
-        {/* <NavItem title="Careers" ExpandedElem={<NavDetails />} /> */}
-      </div>
-      <div className="right-bar">
-        <Search />
-        <div className="nav-btn">Contact</div>
+      <div className="mainWrapper">
+        <div className="logo">
+          <Image alt="Logo" src={Logo} />
+          LEGiON Platfoms
+        </div>
+        <div className="links">
+          <NavItem title="Products" ExpandedElem={<NavDetails />} />
+          <NavItem title="Company" ExpandedElem={<NavDetails />} />
+          <NavItem title="Resources" ExpandedElem={<NavDetails />} />
+          {/* <NavItem title="Careers" ExpandedElem={<NavDetails />} /> */}
+        </div>
+        <div className="right-bar">
+          <Search />
+          <div className="nav-btn">Contact</div>
+        </div>
       </div>
     </div>
   );
