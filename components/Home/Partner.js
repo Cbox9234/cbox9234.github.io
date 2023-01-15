813da204b27ec11f8b4e416ccb887dcc0637b603
epicX67 <souravgain605@gmail.com>
2023-01-15 17:06:24 +0530

    added dark mode partner images

--- components/Home/Partner.js
+++ components/Home/Partner.js
@@ -1,22 +1,25 @@
 import React from "react";
 import poki from "../../res/partners/poki.png";
+import poki_dark from "../../res/partners/poki_dark.png";
 import frvr from "../../res/partners/frvr.png";
+import frvr_dark from "../../res/partners/frvr_dark.png";
 import krunker from "../../res/partners/krunker2.png";
 import yendis from "../../res/partners/yendis.png";
 import Image from "next/image";
 
 export default function Partner() {
-  yendis;
   return (
     <section className="aSection partners">
       <div className="title">Our Partners</div>
       <div className="partnerList">
         <div className="partner-item">
-          <Image src={poki} alt="Poki" />
+          <Image src={poki} alt="Poki" className="light" />
+          <Image src={poki_dark} alt="Poki_dark" className="dark" />
         </div>
 
         <div className="partner-item">
-          <Image src={frvr} alt="Frvr" />
+          <Image src={frvr} alt="Frvr" className="light" />
+          <Image src={frvr_dark} alt="Frvr_dark" className="dark" />
         </div>
         <div className="partner-item">
           <Image src={krunker} alt="Krunker" />
