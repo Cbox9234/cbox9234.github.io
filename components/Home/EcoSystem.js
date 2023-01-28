c10a53c0efbb9c36b4378798e1fa36f004e1c1b3
epicX67 <souravgain605@gmail.com>
2023-01-28 14:15:54 +0530

    changed cards

--- components/Home/EcoSystem.js
+++ components/Home/EcoSystem.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Logo from "../../res/logo.png";
 
 export default function EcoSystem() {
   return (
@@ -12,13 +13,155 @@ export default function EcoSystem() {
         className="card-set"
       >
         <div className="cards">
-          <div className="card"></div>
-          <div className="card"></div>
-          <div className="card"></div>
+          <div className="card">
+            <div className="logo" onClick={() => router.push("/")}>
+              <img
+                alt="Logo"
+                src="https://cdn.discordapp.com/attachments/918131950106083378/1068795423600410684/logo.png"
+              />
+              LEGiON Portal
+            </div>
+            <div className="content">Play thousands of games within clicks</div>
+            <div className="last-set">
+              <button>Explore</button>
+              <div className="rCont">
+                <div className="image one">
+                  <div
+                    style={{
+                      backgroundImage: `url('https://media.discordapp.net/attachments/867587464733130812/993640743891636315/render_4.png')`,
+                    }}
+                  ></div>
+                </div>
+                <div className="image two">
+                  <div
+                    style={{
+                      backgroundImage: `url('https://media.discordapp.net/attachments/1063575553610883182/1063575644476297277/3.png?width=663&height=663')`,
+                    }}
+                  ></div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="card">
+            <div className="logo" onClick={() => router.push("/")}>
+              <img
+                alt="Logo"
+                src="https://cdn.discordapp.com/attachments/918131950106083378/1068795423600410684/logo.png"
+              />
+              LEGiON Distribution
+            </div>
+            <div className="content">Premium games for your products</div>
+            <div className="last-set">
+              <button>Explore</button>
+              <div className="rCont">
+                <div className="image one">
+                  <div
+                    style={{
+                      backgroundImage: `url('https://media.discordapp.net/attachments/867587464733130812/993640743891636315/render_4.png')`,
+                    }}
+                  ></div>
+                </div>
+                <div className="image two">
+                  <div
+                    style={{
+                      backgroundImage: `url('https://media.discordapp.net/attachments/1063575553610883182/1063575644476297277/3.png?width=663&height=663')`,
+                    }}
+                  ></div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="card">
+            <div className="logo" onClick={() => router.push("/")}>
+              <img
+                alt="Logo"
+                src="https://cdn.discordapp.com/attachments/918131950106083378/1068795423600410684/logo.png"
+              />
+              LEGiON Publishing
+            </div>
+            <div className="content">
+              Bring your game or project to millions
+            </div>
+            <div className="last-set">
+              <button>Explore</button>
+              <div className="rCont">
+                <div className="image one">
+                  <div
+                    style={{
+                      backgroundImage: `url('https://media.discordapp.net/attachments/867587464733130812/993640743891636315/render_4.png')`,
+                    }}
+                  ></div>
+                </div>
+                <div className="image two">
+                  <div
+                    style={{
+                      backgroundImage: `url('https://media.discordapp.net/attachments/1063575553610883182/1063575644476297277/3.png?width=663&height=663')`,
+                    }}
+                  ></div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
         <div className="cards">
-          <div className="card"></div>
-          <div className="card"></div>
+          <div className="card">
+            <div className="logo" onClick={() => router.push("/")}>
+              <img
+                alt="Logo"
+                src="https://cdn.discordapp.com/attachments/918131950106083378/1068795423600410684/logo.png"
+              />
+              LEGiON Advertising
+            </div>
+            <div className="content">Advertise your brand to millions</div>
+            <div className="last-set">
+              <button>Explore</button>
+              <div className="rCont">
+                <div className="image one">
+                  <div
+                    style={{
+                      backgroundImage: `url('https://media.discordapp.net/attachments/867587464733130812/993640743891636315/render_4.png')`,
+                    }}
+                  ></div>
+                </div>
+                <div className="image two">
+                  <div
+                    style={{
+                      backgroundImage: `url('https://media.discordapp.net/attachments/1063575553610883182/1063575644476297277/3.png?width=663&height=663')`,
+                    }}
+                  ></div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="card">
+            <div className="logo" onClick={() => router.push("/")}>
+              <img
+                alt="Logo"
+                src="https://cdn.discordapp.com/attachments/918131950106083378/1068795423600410684/logo.png"
+              />
+              LEGiON White Label
+            </div>
+            <div className="content">Premium white label for your products</div>
+            <div className="last-set">
+              <button>Explore</button>
+              <div className="rCont">
+                <div className="image one">
+                  <div
+                    style={{
+                      backgroundImage: `url('https://media.discordapp.net/attachments/867587464733130812/993640743891636315/render_4.png')`,
+                    }}
+                  ></div>
+                </div>
+                <div className="image two">
+                  <div
+                    style={{
+                      backgroundImage: `url('https://media.discordapp.net/attachments/1063575553610883182/1063575644476297277/3.png?width=663&height=663')`,
+                    }}
+                  ></div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </section>
