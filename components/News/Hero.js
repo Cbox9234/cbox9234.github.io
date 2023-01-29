b308eddc442017287aef005bc46c415000f65d68
epicX67 <souravgain605@gmail.com>
2023-01-29 22:05:28 +0530

    normalized each news page

--- components/News/Hero.js
+++ components/News/Hero.js
@@ -1,16 +1,17 @@
-import Image from "next/image";
+/* eslint-disable @next/next/no-img-element */
 import React from "react";
-import Cover from "../../res/news/big.png";
 
-export default function Hero() {
+export default function Hero({ data }) {
   return (
     <section className="aSection newsHero policiesHero genericHero2">
       <div className="mainCont">
-        <div className="sub-title">LEGiON PRESS RELEASE</div>
-        <div className="title">Plundur Beta launch</div>
-        <div className="sub-title">December 12th, 2021</div>
+        <div className="sub-title">{data.pageType}</div>
+        <div className="title">
+          {data.shortTitle !== "blank" ? data.shortTitle : data.title}
+        </div>
+        <div className="sub-title">{data.date}</div>
         <div className="coverCont">
-          <Image className="cover" src={Cover} alt="Cover"></Image>
+          <img className="cover" src={data.coverImage} alt="Cover"></img>
         </div>
       </div>
     </section>
--- pages/news/[title].js
+++ pages/news/[title].js
@@ -3,42 +3,44 @@ import React from "react";
 import Footer from "../../components/Footer/Footer";
 import Nav from "../../components/Nav";
 import Hero from "../../components/News/Hero";
+import newsList from "../../res/news.json";
 
-export default function News() {
+export async function getStaticPaths() {
+  const myPaths = newsList.map((item, index) => ({
+    params: { title: item.shortTitle },
+  }));
+  return {
+    paths: [...myPaths],
+    fallback: false, // can also be true or 'blocking'
+  };
+}
+
+// `getStaticPaths` requires using `getStaticProps`
+export async function getStaticProps(context) {
+  const news = newsList.find(
+    (item) => item.shortTitle === context.params.title
+  );
+  return {
+    // Passed to the page component as props
+    props: { news: news },
+  };
+}
+
+export default function News({ news }) {
   return (
     <>
       <Head>
-        <title>Legion Platforms - News</title>
+        <title>{news.shortTitle}</title>
         <meta name="description" content="Legion Platforms" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href={news.thumbnail} />
       </Head>
       <Nav />
       <main className="termsofservice">
         <div className="mainWrapper termsofservice">
-          <Hero />
+          <Hero data={news} />
           <div className="newsTextSet">
-            <div className="info">
-              LEGiON Platforms is committed to conducting business in accordance
-              with the highest ethical standards. The following policy serves as
-              a guide to help all employees make responsible and ethical
-              decisions. Confidentiality: LEGiON Platforms recognizes the
-              importance of protecting the confidential information of our
-              clients, partners, and employees. All parties are expected to
-              maintain the confidentiality of any sensitive information shared
-              in the course of business. Fair Treatment: LEGiON Platforms is
-              committed to treating all customers and clients with fairness and
-              impartiality. We will not discriminate against any person on the
-              basis of race, religion, gender, sexual orientation, age, or any
-              other characteristic protected by law. Respect for Others: LEGiON
-              Platforms expects all employees to respect the rights and dignity
-              of all individuals, including clients, employees, partners, and
-              competitors. Any form of harassment, discrimination or
-              disrespectful behavior will not be tolerated. Global Reach: LEGiON
-              Platforms serves a massive online customer base across the world.
-              We are committed to maintaining the highest standards of business
-              conduct in all the countries and regions in which we operate.
-            </div>
+            <div className="info">{news.article}</div>
           </div>
           <Footer />
         </div>
