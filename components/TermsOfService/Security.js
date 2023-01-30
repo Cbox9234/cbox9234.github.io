489e92dbd9f94847bd15bd644c1cd2112740dbe7
epicX67 <souravgain605@gmail.com>
2023-01-30 22:08:31 +0530

    fixed spacing for security page

--- components/TermsOfService/Security.js
+++ components/TermsOfService/Security.js
@@ -12,13 +12,13 @@ export default function Security() {
         We also closely monitor for any potential threats and take immediate
         action to address them. Additionally, we require all employees and
         contractors to adhere to strict security guidelines to ensure the
-        protection of our users information. As a company, we take user safety
-        very seriously and will continue to prioritize security in all of our
-        operations. We are dedicated to providing a secure experience for all
-        users of our products and services.
-        <br></br>
-        <br></br>
-        <br></br>
+        protection of our users information.
+      </div>
+      <div className="info">
+        As a company, we take user safety very seriously and will continue to
+        prioritize security in all of our operations. We are dedicated to
+        providing a secure experience for all users of our products and
+        services.
       </div>
     </div>
   );
