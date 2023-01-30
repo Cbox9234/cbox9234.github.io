45f97917e054aad2011ad1f84198d122e5a43329
epicX67 <souravgain605@gmail.com>
2023-01-30 22:07:41 +0530

    fixed spacing of help content

--- components/TermsOfService/Help.js
+++ components/TermsOfService/Help.js
@@ -8,20 +8,30 @@ export default function Help() {
         At LEGiON Platforms, we are committed to providing our customers with
         the best possible experience when using our products and services. We
         understand that sometimes, users may encounter issues or have questions
-        that need answering. That's why we're here to help. If you need
-        assistance, you can reach out to us by emailing us at
+        that need answering. That's why we're here to help.`}
+      </div>
+
+      <div className="info">
+        {`If you need assistance, you can reach out to us by emailing us at
         legionplatforms@gmail.com. Our dedicated support team will be more than
-        happy to assist you with any issues you may be facing. We value your
-        feedback and are always looking for ways to improve our products and
+        happy to assist you with any issues you may be facing. 
+        
+        `}
+      </div>
+      <div className="info">
+        {`
+        We value your feedback and are always looking for ways to improve our products and
         services. If you have any suggestions or ideas, please don't hesitate to
         let us know. We're here to help, and we're always happy to hear from our
-        customers. Thank you for choosing LEGiON Platforms. We look forward to
+        customers. 
+        `}
+      </div>
+      <div className="info">
+        {`
+        Thank you for choosing LEGiON Platforms. We look forward to
         serving you and ensuring that your experience with our products and
         services is a positive one.
         `}
-        <br></br>
-        <br></br>
-        <br></br>
       </div>
     </div>
   );
