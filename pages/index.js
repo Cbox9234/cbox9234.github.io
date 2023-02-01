0f538c242506ef3f50dbc6082c07b48a2cff2f37
epicX67 <souravgain605@gmail.com>
2023-02-01 10:11:35 +0530

    changed text in home hero section

--- pages/index.js
+++ pages/index.js
@@ -49,7 +49,8 @@ export default function Home() {
             <div className="lCont">
               <div className="title">Experiences that matter.</div>
               <div className="subTitle">
-                We are building next generation of web/mobile technology
+                We are building the next generation of <br></br> web/mobile
+                technology
               </div>
               <div className="heroBtnSet">
                 <button onClick={() => gotoExplore()} className="heroBtn">
