fac1af3ce2f1a07cf725a826236e3ad2db26ae6a
epicX67 <souravgain605@gmail.com>
2024-03-24 20:34:17 +0530

    updated partner section in homepage

--- components/Home/Partnership.js
+++ components/Home/Partnership.js
@@ -1,15 +1,18 @@
 import Image from "next/image";
 import React from "react";
 import earth from "../../res/earth.png";
+import { useRouter } from "next/router";
 
 export default function Partnership() {
+  const router = useRouter();
+
   return (
     <section className="aSection partnership">
       <div className="title">
-        <span>Partner</span> With LEGiON
+        <span>Partner</span> With Us
       </div>
       <div className="info">Take your company/project to new heights.</div>
-      <button>Apply For Partnership</button>
+      <button onClick={() => router.push("/partnership")}>Apply For Partnership</button>
 
       <Image src={earth} alt="earth" />
     </section>
