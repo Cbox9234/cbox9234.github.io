1c459b11ccb1e30ab7218f7908ad2863bad6c7c4
Sourav Gain <souravgain605@gmail.com>
2024-10-06 23:49:22 +0530

    updated from 4 million to 8 million users

--- components/Home/Users.js
+++ components/Home/Users.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Users() {
   return (
     <section className="aSection users">
-      <div className="title">Over 4 Million People</div>
+      <div className="title">Over 8 Million People</div>
       <div className="title">Use LEGiON Products</div>
       <div className="info">Across every country in the world.</div>
     </section>
--- pages/index.js
+++ pages/index.js
@@ -49,7 +49,7 @@ export default function Home() {
             <div className="lCont">
               <div className="title">Experiences that matter.</div>
               <div className="subTitle">
-                We create games and experiences that <br></br> are loved by 4 million users worldwide.
+                We create games and experiences that <br></br> are loved by 8 million users worldwide.
               </div>
               <div className="heroBtnSet">
                 <button onClick={() => gotoExplore()} className="heroBtn">
